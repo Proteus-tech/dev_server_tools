56eef9296a212a1a1966f9ca0d25bb6648774620
Chanita Siridechkun Anuwong <aireanalistic@gmail.com>
2011-06-29 00:13:59 +0700

    Add code

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='dev_server_tools',
+      version='1.0',
+      description='dev server tools for Django',
+      author='Proteus Technologies',
+      author_email='team@proteus-tech.com',
+      url='http://proteus-tech.com',
+      packages=['dev_server_tools',],
+     )
