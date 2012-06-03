b15e03c6434be6642b46667de3cae3b693d45546
Pam Suebvisai <sinapam@proteus-tech.com>
2012-06-04 01:06:49 +0700

    update dev_server_tools to support django 1.3 style

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='dev_server_tools',
-      version='1.0',
+      version='1.1',
       description='dev server tools for Django',
       author='Proteus Technologies',
       author_email='team@proteus-tech.com',
